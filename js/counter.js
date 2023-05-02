2e95dfc2c35990e1de5cb41388b041529e148fe0
Victor Golovin <rinoally@gmail.com>
2023-05-02 20:11:55 +1000

    edit file and change font famaly for button

--- js/counter.js
+++ js/counter.js
@@ -1,16 +1,16 @@
-let buttonCounter = document.querySelector('.counter-btn');
-let buttonReset = document.querySelector('.counter-btn-reset');
-let counterNumber = document.querySelector('.counter-number');
+let buttonCounter = document.querySelector(".counter-btn");
+let buttonReset = document.querySelector(".counter-btn-reset");
+let counterNumber = document.querySelector(".counter-number");
 let counter = 0;
 
-buttonCounter.addEventListener('click', function() {
-    counter = counter + 1;
+buttonCounter.addEventListener("click", function () {
+  counter = counter + 1;
 
-    counterNumber.innerText = counter;
+  counterNumber.innerText = counter;
 });
 
-buttonReset.addEventListener('click', function() {
-    counter = counter * 0;
+buttonReset.addEventListener("click", function () {
+  counter = counter * 0;
 
-    counterNumber.innerText = counter;
+  counterNumber.innerText = counter;
 });
