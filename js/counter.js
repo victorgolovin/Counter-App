5e9becfde592ddd0a9edd76a8412c72c04c1bee4
victorgolovin <124113149+victorgolovin@users.noreply.github.com>
2023-05-02 19:56:13 +1000

    Add files via upload

---
+++ js/counter.js
@@ -0,0 +1,16 @@
+let buttonCounter = document.querySelector('.counter-btn');
+let buttonReset = document.querySelector('.counter-btn-reset');
+let counterNumber = document.querySelector('.counter-number');
+let counter = 0;
+
+buttonCounter.addEventListener('click', function() {
+    counter = counter + 1;
+
+    counterNumber.innerText = counter;
+});
+
+buttonReset.addEventListener('click', function() {
+    counter = counter * 0;
+
+    counterNumber.innerText = counter;
+});
